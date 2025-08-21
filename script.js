250e68aaab0590cbf506734456dd176fbaed4f3b
Jayasreerathod <rathodprvnkmr@gmail.com>
2025-08-21 19:28:44 +0530

    Replace hardcoded API key with environment variable

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const themeToggle = document.querySelector("#theme-toggle-btn");
 
 
 // API setup
-const API_KEY ="AIzaSyDhlXA3k5ngDNaOXcF9VP9519_-cWVpnwA";
+const API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${API_KEY}`;
 
 let typingInterval, controller;
